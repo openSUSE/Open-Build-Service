4437edac3f2f46ad696da3934a08c643329d949a
Adrian Schröter <adrian@suse.de>
2019-07-19 16:38:04 +0200

    [api] allow to enforce maintenance request staging
    To create or to enforce staged maintenace submissions as part of
    maintenance_incident request actions.
    
    This can be used either on demand when creating the request by
    user or can be enforced in the maintenance project by creating
    OBS:EnforceIncidentRequestStaging attribute
    
    OBS-73

--- src/api/app/models/bs_request.rb
+++ src/api/app/models/bs_request.rb
@@ -276,6 +276,10 @@ class BsRequest < ApplicationRecord
     @ignore_delegate = true
   end
 
+  def enforce_branching
+    @enforce_branching = true
+  end
+
   def sanitize?
     !@skip_sanitize
   end
@@ -897,6 +901,8 @@ class BsRequest < ApplicationRecord
     check_bs_request_actions!
     check_uniq_actions!
 
+    modify_sources
+
     # Autoapproval? Is the creator allowed to accept it?
     permission_check_change_state!(newstate: 'accepted') if accept_at
 
@@ -933,6 +939,12 @@ class BsRequest < ApplicationRecord
       end
     end
     actions
+  end
+
+  def modify_sources
+    bs_request_actions.each do |action|
+      action.modify_sources(@enforce_branching)
+    end
   end
 
   def expand_targets
