4f41e2ac397202361771afd594bb752682760fd3
Adrian Schröter <adrian@suse.de>
2020-08-12 08:40:04 +0200

    [api] support owner lookup based on package container name
    Fixes situations where no binary is named like the container
    name (in some multibuild situations).
    
    Allows also faster lookups in best case.

--- src/api/test/functional/search_controller_test.rb
+++ src/api/test/functional/search_controller_test.rb
@@ -1,6 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/..') + '/test_helper'
 require 'xmlhash'
 
+# rubocop:disable Metrics/MethodLength
+
 class SearchControllerTest < ActionDispatch::IntegrationTest
   fixtures :all
 
@@ -426,8 +428,8 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
     run_publisher
 
     get "/search/owner?binary='package'"
-    assert_response 404
-    assert_xml_tag tag: 'status', attributes: { code: 'unknown_project' }
+    assert_response 400
+    assert_xml_tag tag: 'status', attributes: { code: 'attribute_not_set' }
 
     get "/search/owner?binary='package'&attribute='OBS:does_not_exist'"
     assert_response 404
@@ -436,6 +438,7 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
     post '/source/home:Iggy/_attribute', params: "<attributes><attribute namespace='OBS' name='OwnerRootProject' /></attributes>"
     assert_response :success
 
+    # search via binaries
     get '/search/owner?binary=DOES_NOT_EXIST'
     assert_response :success
     assert_xml_tag tag: 'collection', children: { count: 0 }
@@ -452,6 +455,18 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
     assert_xml_tag tag: 'owner', attributes: { rootproject: 'home:Iggy', project: 'home:Iggy', package: 'TestPack' }
     assert_no_xml_tag tag: 'person', attributes: { name: 'fred', role: 'maintainer' }
     assert_no_xml_tag tag: 'person', attributes: { name: 'Iggy', role: 'maintainer' }
+    assert_xml_tag tag: 'person', attributes: { name: 'Iggy', role: 'bugowner' }
+
+    # search via package container name
+    get '/search/owner?package=DOES_NOT_EXIST'
+    assert_response :success
+    assert_xml_tag tag: 'collection', children: { count: 0 }
+
+    get '/search/owner?package=TestPack'
+    assert_response :success
+    assert_xml_tag tag: 'owner', attributes: { rootproject: 'home:Iggy', project: 'home:Iggy', package: 'TestPack' }
+    assert_xml_tag tag: 'person', attributes: { name: 'fred', role: 'maintainer' }
+    assert_xml_tag tag: 'person', attributes: { name: 'Iggy', role: 'maintainer' }
     assert_xml_tag tag: 'person', attributes: { name: 'Iggy', role: 'bugowner' }
 
     # disable filter
@@ -851,3 +866,5 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
     assert_xml_tag child: { tag: 'project', attributes: { name: 'RemoteInstance' } }
   end
 end
+
+# rubocop:enable Metrics/MethodLength
