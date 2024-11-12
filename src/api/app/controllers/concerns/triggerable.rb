4c6bd1a4c7261dc37bddde01f86990b1f0ca14e0
Eduardo Navarro <enavarro@suse.com>
2024-11-12 15:04:20 +0100

    Fix RuboCop offenses and refactor the code slightly

--- src/api/app/controllers/concerns/triggerable.rb
+++ src/api/app/controllers/concerns/triggerable.rb
@@ -9,26 +9,26 @@ module Triggerable
     raise Project::Errors::UnknownObjectError, "Sorry, triggering tokens for remote project \"#{@project_name}\" is not possible." unless @project.is_a?(Project)
   end
 
-  def set_package(package_find_options: {})
-    package_find_options = @token.package_find_options if package_find_options.blank?
+  def set_package(package_find_options: {}) # rubocop:disable Metrics/CyclomaticComplexity
     # By default we operate on the package association
-    @package = @token.package
-    return if @package
+    return if (@package = @token.package)
+
     # If the token has no package, let's find one from the parameters
     if @package_name.present?
-      @package ||= Package.get_by_project_and_name(@project.name,
-                                                   @package_name,
-                                                   package_find_options)
+      package_find_options = @token.package_find_options if package_find_options.blank?
+
+      return if (@package = Package.get_by_project_and_name(@project.name, @package_name, package_find_options))
     end
-    return if @package
+
     return unless @project.links_to_remote? || @project.scmsync.present?
 
     # In remote or scmsync case we have no database object, but the package
     # may still be there. It will get validated by the backend.
     # Strip multibuild part as the code will add it later again in the model
-    @package ||= @package_name.gsub(/:.*/, '')
+    return if (@package = @package_name.gsub(/:.*/, ''))
+
     # TODO: This should not happen right? But who knows...
-    raise ActiveRecord::RecordNotFound unless @package
+    raise ActiveRecord::RecordNotFound
   end
 
   # FIXME: Use the version from Webui::WebuiController instead
